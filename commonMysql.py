2de712ce8ec7d844ad65af53e7a32ad9cbc6d4f7
Ryan Popa <pope790@gmail.com>
2012-05-17 10:55:16 -0400

    Fixed MySql escaping (i hope)

--- commonMysql.py
+++ commonMysql.py
@@ -10,17 +10,17 @@ def createConnection():
 #TV db functions
 def addSeriesAlias(conn, seriesId, newName):
 	cursor = conn.cursor()
-	cursor.execute("UPDATE TvSeries SET alias = '%s', FileStates_id = 2 WHERE id = %d" % (newName, seriesId))
+	cursor.execute("UPDATE TvSeries SET alias = %s, FileStates_id = 2 WHERE id = %d", (newName, seriesId))
  	conn.commit()
 
 def addPendingSeries(conn, series):
 	cursor = conn.cursor()
-	cursor.execute("INSERT INTO TvSeries (series, FileStates_id) VALUES ('%s', 0)" % (series))
+	cursor.execute("INSERT INTO TvSeries (series, FileStates_id) VALUES (%s, 0)", (series))
 	conn.commit()
 	
 def getTvSeries(conn, series):
 	cursor = conn.cursor()
-	cursor.execute("SELECT * FROM TvSeries WHERE series = '%s'" % (series))
+	cursor.execute("SELECT * FROM TvSeries WHERE series = %s", (series))
 	if cursor.rowcount == 0:
 		return None
 	else:
@@ -37,9 +37,9 @@ def getPendingTvSeries(conn):
 #Movie db functions	
 def addPendingMovie(conn, path):
 	cursor = conn.cursor()
-	cursor.execute("INSERT INTO MovieFiles (path, FileStates_id) VALUES ('%s', 0)" % (path))
+	cursor.execute("INSERT INTO MovieFiles (path, FileStates_id) VALUES (%s, 0)", (path))
 	conn.commit()
-	cursor.execute("SELECT id FROM MovieFiles WHERE path = '%s'" % (path))
+	cursor.execute("SELECT id FROM MovieFiles WHERE path = %s", (path))
 	result = cursor.fetchall()
 	result = result[0][0]
 	return result
@@ -48,14 +48,14 @@ def addImdbTitle(conn, title, movieId):
 	cursor = conn.cursor()
 	try:	
 		t = conn.escape_string(title)
-		cursor.execute("INSERT INTO MovieTitles (MovieFiles_id, title)	VALUES (%d, '%s')" % (movieId, t))
+		cursor.execute("INSERT INTO MovieTitles (MovieFiles_id, title)	VALUES (%d, %s)", (movieId, t))
 		conn.commit()
 	except UnicodeEncodeError:
 		print "Unicode error"
 
 def getMovie(conn, path):
 	cursor = conn.cursor()
-	cursor.execute("SELECT * FROM MovieFiles mf LEFT JOIN MovieTitles mt ON mf.id = mt.MovieFiles_id WHERE mf.path = '%s'" % (path))
+	cursor.execute("SELECT * FROM MovieFiles mf LEFT JOIN MovieTitles mt ON mf.id = mt.MovieFiles_id WHERE mf.path = %s", (path))
 	if cursor.rowcount == 0:
 		return None
 	else:
@@ -71,17 +71,17 @@ def getPendingMovies(conn):
 
 def approveMovie(conn, movieId, titleId):
 	cursor = conn.cursor()
-	cursor.execute("UPDATE MovieFiles SET FileStates_id = 1 WHERE id = %s" % (movieId))
-	cursor.execute("DELETE FROM MovieTitles WHERE MovieFiles_id = %s AND id != %s" % (movieId, titleId))
+	cursor.execute("UPDATE MovieFiles SET FileStates_id = 1 WHERE id = %s", (movieId))
+	cursor.execute("DELETE FROM MovieTitles WHERE MovieFiles_id = %s AND id != %s", (movieId, titleId))
 	conn.commit()
 
 def ignoreMovie(conn, movieId):
 	cursor = conn.cursor()
-	cursor.execute("UPDATE MovieFiles SET FileStates_id = 3 WHERE id = %s" % (movieId))
-	cursor.execute("DELETE FROM MovieTitles WHERE MovieFiles_id = %s" % (movieId))
+	cursor.execute("UPDATE MovieFiles SET FileStates_id = 3 WHERE id = %s", (movieId))
+	cursor.execute("DELETE FROM MovieTitles WHERE MovieFiles_id = %s", (movieId))
 	conn.commit()
 
 def finalizeMovie(conn, movieId):
 	cursor = conn.cursor()
-	cursor.execute("UPDATE MovieFiles SET FileStates_id = 2 WHERE id = %d" % (movieId))
+	cursor.execute("UPDATE MovieFiles SET FileStates_id = 2 WHERE id = %d", (movieId))
 	conn.commit()
