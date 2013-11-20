43474512c56b0fa73d8743fe7e04c37597a4e614
Ryan Popa <pope790@gmail.com>
2013-11-19 18:27:47 -0600

    Fixing removal of special puncuation for windows machines and helping to match file names Fixing bug with one the results reporting lengths incorectly by try catching :-/

--- helpers/mediaLinker.py
+++ helpers/mediaLinker.py
@@ -10,225 +10,225 @@ import helpers.settingsManager as settingsManager
 #Association functions ----------------------------------------------------------
 #--------------------------------------------------------------------------------
 def associateMovieWithMediaFile(aMovie, aMediaFile, conn = None):
-	if conn == None:
-		conn = mySql.createConnection()
-	cursor = conn.cursor()
+  if conn == None:
+    conn = mySql.createConnection()
+  cursor = conn.cursor()
 
-	aMovie.associatedMediaFileId = aMediaFile.id
-	aMovie.save(conn) 
-	return aMovie 
+  aMovie.associatedMediaFileId = aMediaFile.id
+  aMovie.save(conn) 
+  return aMovie 
 
 def associateArrayOfMoviesWithMediaFile(aMovieArray, aMediaFile, conn = None):
-	if conn == None:
-		conn = mySql.createConnection()
-	cursor = conn.cursor()
+  if conn == None:
+    conn = mySql.createConnection()
+  cursor = conn.cursor()
 
-	for m in aMovieArray:
-		associateMovieWithMediaFile(m, aMediaFile, conn)
+  for m in aMovieArray:
+    associateMovieWithMediaFile(m, aMediaFile, conn)
 
 def associateEpisodeWithMediaFile(anEpisode, aMediaFile, conn = None):
-	if conn == None:
-		conn = mySql.createConnection()
-	cursor = conn.cursor()
+  if conn == None:
+    conn = mySql.createConnection()
+  cursor = conn.cursor()
 
-	anEpisode.associatedMediaFileId = aMediaFile.id
-	anEpisode.save(conn)
-	return anEpisode
+  anEpisode.associatedMediaFileId = aMediaFile.id
+  anEpisode.save(conn)
+  return anEpisode
 
 def associateEpisodeWithSeriesAlias(anEpisode, aSeriesAlias, conn = None):
-	if conn == None:
-		conn = mySql.createConnection()
-	cursor = conn.cursor()
+  if conn == None:
+    conn = mySql.createConnection()
+  cursor = conn.cursor()
 
-	anEpisode.associatedSeriesAliasId = aSeriesAlias.id
-	anEpisode.save(conn)
-	return anEpisode
+  anEpisode.associatedSeriesAliasId = aSeriesAlias.id
+  anEpisode.save(conn)
+  return anEpisode
 
 def associateSeriesWithSeriesAlias(aSeries, aSeriesAlias, conn = None):
-	if conn == None:
-		conn = mySql.createConnection()
-	cursor = conn.cursor()
+  if conn == None:
+    conn = mySql.createConnection()
+  cursor = conn.cursor()
 
-	aSeries.associatedSeriesAliasId = aSeriesAlias.id
-	aSeries.save(conn)
-	return aSeries
+  aSeries.associatedSeriesAliasId = aSeriesAlias.id
+  aSeries.save(conn)
+  return aSeries
 
 def associateArrayOfSeriesWithSeriesAlias(aSeriesArray, aSeriesAlias, conn = None):
-	if conn == None:
-		conn = mySql.createConnection()
-	cursor = conn.cursor()
+  if conn == None:
+    conn = mySql.createConnection()
+  cursor = conn.cursor()
 
-	for s in aSeriesArray:
-		associateSeriesWithSeriesAlias(s, aSeriesAlias, conn)
+  for s in aSeriesArray:
+    associateSeriesWithSeriesAlias(s, aSeriesAlias, conn)
 
 #Linker functions ---------------------------------------------------------------
 #--------------------------------------------------------------------------------
 def linkMediaFileToMovie(aMediaFile, aMovie, conn = None):
-	if conn == None:
-		conn = mySql.createConnection()
-	cursor = conn.cursor()
+  if conn == None:
+    conn = mySql.createConnection()
+  cursor = conn.cursor()
 
-	dirConf = settingsManager.directorySettings()
+  dirConf = settingsManager.directorySettings()
 
-	#Set the proper movie as 'Active'
-	movieArray = movie.getByMediaFileId(aMediaFile.id, conn)
-	for m in movieArray:
-		m.active = 0
-		if m.id == aMovie.id:
-			m.active = 1
-		m.save(conn)
+  #Set the proper movie as 'Active'
+  movieArray = movie.getByMediaFileId(aMediaFile.id, conn)
+  for m in movieArray:
+    m.active = 0
+    if m.id == aMovie.id:
+      m.active = 1
+    m.save(conn)
 
-	#Delete the existing linked file if there is one
-	removeHardLinkForMediaFile(aMediaFile)
+  #Delete the existing linked file if there is one
+  removeHardLinkForMediaFile(aMediaFile)
 
-	#Generate a new linked file name and path and update the media file accordingly
-	movieFileName = generateHardLinkNameFromMediaFileAndMovie(aMediaFile, aMovie)
-	moviePath = os.path.join(dirConf.movieDestination, movieFileName)
-	aMediaFile.linkedPath = moviePath
-	aMediaFile.save(conn)
+  #Generate a new linked file name and path and update the media file accordingly
+  movieFileName = generateHardLinkNameFromMediaFileAndMovie(aMediaFile, aMovie)
+  moviePath = os.path.join(dirConf.movieDestination, movieFileName)
+  aMediaFile.linkedPath = moviePath
+  aMediaFile.save(conn)
 
-	#Create the new link
-	createHardLinkForMediaFile(aMediaFile)
+  #Create the new link
+  createHardLinkForMediaFile(aMediaFile)
 
-	return movieFileName
+  return movieFileName
 
 def linkMediaFileToSeries(aMediaFile, aSeries, conn = None):
-	if conn == None:
-		conn = mySql.createConnection()
-	cursor = conn.cursor()
+  if conn == None:
+    conn = mySql.createConnection()
+  cursor = conn.cursor()
 
-	dirConf = settingsManager.directorySettings()
+  dirConf = settingsManager.directorySettings()
 
-	#Get all the episode information needed to do the linking
-	anEpisode = episode.getByMediaFileId(aMediaFile.id, conn)
-	aSeriesAlias = seriesAlias.getBySeriesAliasId(anEpisode.associatedSeriesAliasId, conn)
+  #Get all the episode information needed to do the linking
+  anEpisode = episode.getByMediaFileId(aMediaFile.id, conn)
+  aSeriesAlias = seriesAlias.getBySeriesAliasId(anEpisode.associatedSeriesAliasId, conn)
 
-	#Set the proper series as 'Active'
-	seriesArray = series.getBySeriesAliasId(aSeriesAlias.id, conn)
-	for s in seriesArray:
-		s.active = 0
-		if s.id == aSeries.id:
-			s.active = 1
-		s.save(conn)
+  #Set the proper series as 'Active'
+  seriesArray = series.getBySeriesAliasId(aSeriesAlias.id, conn)
+  for s in seriesArray:
+    s.active = 0
+    if s.id == aSeries.id:
+      s.active = 1
+    s.save(conn)
 
-	#Delete the existing linked file if there is one
-	removeHardLinkForMediaFile(aMediaFile)
-	removeSeasonFolderIfEmptyForMediaFile(aMediaFile)
-	removeSeriesFolderIfEmptyForMediaFile(aMediaFile)
+  #Delete the existing linked file if there is one
+  removeHardLinkForMediaFile(aMediaFile)
+  removeSeasonFolderIfEmptyForMediaFile(aMediaFile)
+  removeSeriesFolderIfEmptyForMediaFile(aMediaFile)
 
-	#Generate a new linked file name and path and update the media file accordingly
-	episodeFileName = generateHardLinkNameFromMediaFileAndEpisodeAndSeries(aMediaFile, anEpisode, aSeries)
-	seasonPath = generateSeasonPathFromEpisodeAndSeries(anEpisode, aSeries)
-	episodePath = os.path.join(seasonPath, episodeFileName)
-	aMediaFile.linkedPath = episodePath
-	aMediaFile.save(conn)
+  #Generate a new linked file name and path and update the media file accordingly
+  episodeFileName = generateHardLinkNameFromMediaFileAndEpisodeAndSeries(aMediaFile, anEpisode, aSeries)
+  seasonPath = generateSeasonPathFromEpisodeAndSeries(anEpisode, aSeries)
+  episodePath = os.path.join(seasonPath, episodeFileName)
+  aMediaFile.linkedPath = episodePath
+  aMediaFile.save(conn)
 
-	#Create the new link
-	createSeriesFolderForMediaFile(aMediaFile)
-	createSeasonFolderForMediaFile(aMediaFile)
-	createHardLinkForMediaFile(aMediaFile)
+  #Create the new link
+  createSeriesFolderForMediaFile(aMediaFile)
+  createSeasonFolderForMediaFile(aMediaFile)
+  createHardLinkForMediaFile(aMediaFile)
 
-	return episodeFileName
+  return episodeFileName
 
 def removeHardLinkForMediaFile(aMediaFile):
-	if aMediaFile.linkedPath != None:
-		moviePath = aMediaFile.linkedPath
-		if os.path.exists(moviePath):
-			os.remove(moviePath)
-	else:
-		return Exception
+  if aMediaFile.linkedPath != None:
+    moviePath = aMediaFile.linkedPath
+    if os.path.exists(moviePath):
+      os.remove(moviePath)
+  else:
+    return Exception
 
 def removeSeasonFolderIfEmptyForMediaFile(aMediaFile):
-	if aMediaFile.linkedPath != None:
-		seasonPath = os.path.dirname(aMediaFile.linkedPath)
-		if os.path.exists(seasonPath):
-			if os.listdir(seasonPath) == []:
-				os.rmdir(seasonPath)
+  if aMediaFile.linkedPath != None:
+    seasonPath = os.path.dirname(aMediaFile.linkedPath)
+    if os.path.exists(seasonPath):
+      if os.listdir(seasonPath) == []:
+        os.rmdir(seasonPath)
 
 def removeSeriesFolderIfEmptyForMediaFile(aMediaFile):
-	if aMediaFile.linkedPath != None:
-		seasonPath = os.path.dirname(aMediaFile.linkedPath)
-		seriesPath = os.path.dirname(seasonPath)
-		if os.path.exists(seriesPath):
-			if os.listdir(seriesPath) == []:
-				os.rmdir(seriesPath)
+  if aMediaFile.linkedPath != None:
+    seasonPath = os.path.dirname(aMediaFile.linkedPath)
+    seriesPath = os.path.dirname(seasonPath)
+    if os.path.exists(seriesPath):
+      if os.listdir(seriesPath) == []:
+        os.rmdir(seriesPath)
 
 def createHardLinkForMediaFile(aMediaFile):
-	if aMediaFile.linkedPath != None:
-		fullPath = aMediaFile.path
-		moviePath = aMediaFile.linkedPath
-		if not os.path.exists(moviePath):
-			os.link(fullPath, moviePath)
-	else:
-		return Exception
+  if aMediaFile.linkedPath != None:
+    fullPath = aMediaFile.path
+    moviePath = aMediaFile.linkedPath
+    if not os.path.exists(moviePath):
+      os.link(fullPath, moviePath)
+  else:
+    return Exception
 
 def createSeriesFolderForMediaFile(aMediaFile):
-	if aMediaFile.linkedPath != None:
-		seasonPath = os.path.dirname(aMediaFile.linkedPath)
-		seriesPath = os.path.dirname(seasonPath)
-		if not os.path.exists(seriesPath):
-			os.makedirs(seriesPath)
+  if aMediaFile.linkedPath != None:
+    seasonPath = os.path.dirname(aMediaFile.linkedPath)
+    seriesPath = os.path.dirname(seasonPath)
+    if not os.path.exists(seriesPath):
+      os.makedirs(seriesPath)
 
 def createSeasonFolderForMediaFile(aMediaFile):
-	if aMediaFile.linkedPath != None:
-		seasonPath = os.path.dirname(aMediaFile.linkedPath)
-		if not os.path.exists(seasonPath):
-			os.makedirs(seasonPath)
+  if aMediaFile.linkedPath != None:
+    seasonPath = os.path.dirname(aMediaFile.linkedPath)
+    if not os.path.exists(seasonPath):
+      os.makedirs(seasonPath)
 
 #File naming functions ----------------------------------------------------------
 #--------------------------------------------------------------------------------
 def generateHardLinkNameFromMediaFileAndMovie(aMediaFile, aMovie):
-	if aMediaFile != None:
-		#Get the parts of the file name i need
-		title = aMovie.title.replace(':', '')
-		year = aMovie.year
-		quality = generateQualityFromMediaFile(aMediaFile)
-		extension = generateExtensionFromMediaFile(aMediaFile)
+  if aMediaFile != None:
+    #Get the parts of the file name i need
+    title = aMovie.title
+    year = aMovie.year
+    quality = generateQualityFromMediaFile(aMediaFile)
+    extension = generateExtensionFromMediaFile(aMediaFile)
 
-		#Genereate the file name skeleton depending on if the quality is HD
-		fileNameSkeleton = '{0} ({1}){3}' if quality == '' else '{0} ({1}) [{2}]{3}'
+    #Genereate the file name skeleton depending on if the quality is HD
+    fileNameSkeleton = '{0} ({1}){3}' if quality == '' else '{0} ({1}) [{2}]{3}'
 
-		return fileNameSkeleton.format(title, year, quality, extension)
-	else:
-		return Exception
+    return fileNameSkeleton.format(title, year, quality, extension)
+  else:
+    return Exception
 
 def generateHardLinkNameFromMediaFileAndEpisodeAndSeries(aMediaFile, anEpisode, aSeries):
-	if aMediaFile != None:
-		#Get the parts of the file name i need
-		series = aSeries.title
-		season = anEpisode.season
-		episode = anEpisode.episode
-		quality = generateQualityFromMediaFile(aMediaFile)
-		extension = generateExtensionFromMediaFile(aMediaFile)
+  if aMediaFile != None:
+    #Get the parts of the file name i need
+    series = aSeries.title
+    season = anEpisode.season
+    episode = anEpisode.episode
+    quality = generateQualityFromMediaFile(aMediaFile)
+    extension = generateExtensionFromMediaFile(aMediaFile)
 
-		#Generate the file name skeleton
-		episodeSkeleton = '0{2}' if anEpisode.episode <= 9 else '{2}'
-		seasonSkeleton = '0{1}' if anEpisode.season <= 9 else '{1}'
-		qualitySkeleton = '{3}' if quality == '' else ' [{3}]'
-		fileNameSkeleton = '{0} - '+seasonSkeleton+'x'+episodeSkeleton+qualitySkeleton+'{4}'
+    #Generate the file name skeleton
+    episodeSkeleton = '0{2}' if anEpisode.episode <= 9 else '{2}'
+    seasonSkeleton = '0{1}' if anEpisode.season <= 9 else '{1}'
+    qualitySkeleton = '{3}' if quality == '' else ' [{3}]'
+    fileNameSkeleton = '{0} - '+seasonSkeleton+'x'+episodeSkeleton+qualitySkeleton+'{4}'
 
-		return fileNameSkeleton.format(series, season, episode, quality, extension)
-	else:
-		return Exception
+    return fileNameSkeleton.format(series, season, episode, quality, extension)
+  else:
+    return Exception
 
 def generateSeasonPathFromEpisodeAndSeries(anEpisode, aSeries):
-	dirConf = settingsManager.directorySettings()
-	seriesPath = os.path.join(dirConf.tvDestination, aSeries.title)
+  dirConf = settingsManager.directorySettings()
+  seriesPath = os.path.join(dirConf.tvDestination, aSeries.title)
 
-	seasonFolderSkeleton = 'Season 0{0}' if anEpisode.season <= 9 else 'Season {0}'
+  seasonFolderSkeleton = 'Season 0{0}' if anEpisode.season <= 9 else 'Season {0}'
 
-	seasonPath = os.path.join(seriesPath, seasonFolderSkeleton.format(anEpisode.season))
-	return seasonPath
+  seasonPath = os.path.join(seriesPath, seasonFolderSkeleton.format(anEpisode.season))
+  return seasonPath
 
 def generateQualityFromMediaFile(aMediaFile):
-	hd = ''
-	path = aMediaFile.path
-	if (path.find('720') != -1):
-		hd ='720'
-	elif (path.find('1080') != -1):
-		hd = '1080'
-	return hd
+  hd = ''
+  path = aMediaFile.path
+  if (path.find('720') != -1):
+    hd ='720'
+  elif (path.find('1080') != -1):
+    hd = '1080'
+  return hd
 
 def generateExtensionFromMediaFile(aMediaFile):
-	path = aMediaFile.path
-	return path[path.rfind('.'):]
+  path = aMediaFile.path
+  return path[path.rfind('.'):]
--- helpers/titleHandler.py
+++ helpers/titleHandler.py
@@ -3,132 +3,138 @@ import re, string
 #String level formating functions -----------------------------------------------
 #--------------------------------------------------------------------------------
 def removePunctuation(title):
-	title = re.sub('[%s]' % re.escape(string.punctuation), ' ', title)
-	return title
+  title = re.sub('[%s]' % re.escape(string.punctuation), ' ', title)
+  return title
+
+def removeWindowsOffendingPunctuation(title):
+  title = title.replace('\'', '')
+  title = title.replace(':', '')
+  return title
 
 def normalizeCase(title):
-	return title.lower()
+  return title.lower()
 
 def replaceAbbreviations(title):
-	return title
+  return title
 
 def removeBlacklistedWords(title):
-	title = title.replace('demonoid.me', '')
-	title = title.replace('avchd', '')
-	title = title.replace('++demonoid.me++', '')
-	return title
+  title = title.replace('demonoid.me', '')
+  title = title.replace('avchd', '')
+  title = title.replace('++demonoid.me++', '')
+  return title
 
 def returnWellFormatedArrayFromTitle(title):
-	title = normalizeCase(title)
-	title = removeBlacklistedWords(title)
-	title = removePunctuation(title)
-	title = replaceAbbreviations(title)
-	return title.split()
+  title = normalizeCase(title)
+  title = removeBlacklistedWords(title)
+  title = removeWindowsOffendingPunctuation(title)
+  title = removePunctuation(title)
+  title = replaceAbbreviations(title)
+  return title.split()
 
 #Array level formating functions ------------------------------------------------
 #--------------------------------------------------------------------------------
 def replaceNumeralsInArray(titleArray):
-	if 'ii' in titleArray:
-		titleArray[titleArray.index('ii')] = '2'
-	if 'iii' in titleArray:
-		titleArray[titleArray.index('iii')] = '3'
-	if 'iv' in titleArray:
-		titleArray[titleArray.index('iv')] = '4'
-	return titleArray
+  if 'ii' in titleArray:
+    titleArray[titleArray.index('ii')] = '2'
+  if 'iii' in titleArray:
+    titleArray[titleArray.index('iii')] = '3'
+  if 'iv' in titleArray:
+    titleArray[titleArray.index('iv')] = '4'
+  return titleArray
 
 def removeLeadingTheInArray(titleArray):
-	if titleArray[0].lower() == 'the':
-		del titleArray[0]
-	return titleArray
+  if titleArray[0].lower() == 'the':
+    del titleArray[0]
+  return titleArray
 
 #Name ordering functions --------------------------------------------------------
 #--------------------------------------------------------------------------------
-def orderMovieArrayByMatchingTitle(movieArray, title):
-	sortedMovies = []
-	for m in movieArray:
-		pMatch = percentageOfTitleMatch(m.title, title)
-		sortedMovies.append([pMatch, m])	
-	sortedMovies.sort(reverse=True)
-	sortedMovies = resolveTies(sortedMovies, title)
+def orderMovieArrayByMatchingTitle(movieArray, title, originalFileName):
+  sortedMovies = []
+  for m in movieArray:
+    pMatch = percentageOfTitleMatch(m.title, title)
+    sortedMovies.append([pMatch, m])  
+  sortedMovies.sort(reverse=True)
+  sortedMovies = resolveTies(sortedMovies, originalFileName)
 
-	returnMovies = []
-	for m in sortedMovies:
-		returnMovies.append(m[1])
-	return returnMovies
+  returnMovies = []
+  for m in sortedMovies:
+    returnMovies.append(m[1])
+  return returnMovies
 
 def orderTvArrayByMatchingSeries(seriesArray, aSeries):
-	sortedSeries = []
-	for s in seriesArray:
-		pMatch = percentageOfTitleMatch(s.title, aSeries)
-		sortedSeries.append([pMatch, s])
-		sortedSeries.sort(reverse=True)
-	returnSeries = []
-	for s in sortedSeries:
-		returnSeries.append(s[1])
-	return returnSeries
+  sortedSeries = []
+  for s in seriesArray:
+    pMatch = percentageOfTitleMatch(s.title, aSeries)
+    sortedSeries.append([pMatch, s])
+    sortedSeries.sort(reverse=True)
+  returnSeries = []
+  for s in sortedSeries:
+    returnSeries.append(s[1])
+  return returnSeries
 
 #Debug printing functions -------------------------------------------------------
 #--------------------------------------------------------------------------------
 def printPercentageArray(percentageMatchedMovieArray, sortedPercentageMatchedMovieArray, reorderedMatchedMovieArray):
-	movieFormat = '{0} {1} ({2})'
-	for i in range(0, len(percentageMatchedMovieArray)):
-		us = percentageMatchedMovieArray[i]
-		s = sortedPercentageMatchedMovieArray[i]
-		ro = reorderedMatchedMovieArray[i]
-		usf = movieFormat.format(round(us[0], 2), us[1].title, us[1].year)
-		sf = movieFormat.format(round(s[0], 2), s[1].title, s[1].year)
-		rof = movieFormat.format(round(ro[0], 2), ro[1].title, ro[1].year)
+  movieFormat = '{0} {1} ({2})'
+  for i in range(0, len(percentageMatchedMovieArray)):
+    us = percentageMatchedMovieArray[i]
+    s = sortedPercentageMatchedMovieArray[i]
+    ro = reorderedMatchedMovieArray[i]
+    usf = movieFormat.format(round(us[0], 2), us[1].title, us[1].year)
+    sf = movieFormat.format(round(s[0], 2), s[1].title, s[1].year)
+    rof = movieFormat.format(round(ro[0], 2), ro[1].title, ro[1].year)
 
-		print usf, ' ' * (50 - len(usf)), sf, ' ' * (50 - len(sf)), rof 
+    print usf, ' ' * (50 - len(usf)), sf, ' ' * (50 - len(sf)), rof 
 
 #Ranking algorithm functions ----------------------------------------------------
 #--------------------------------------------------------------------------------
 #Breaks ties by promoting movies that are in the right year
-def resolveTies(percentageMatchedMovieArray, title):
-	tiedMovies = []
-	for i in range(0, len(percentageMatchedMovieArray)):
-		if i+1 < len(percentageMatchedMovieArray):
-			thisMovie = percentageMatchedMovieArray[i]
-			nextMovie = percentageMatchedMovieArray[i+1]
-			tiedMovies.append(thisMovie)
-			if thisMovie[0] != nextMovie[0]:
-				break
-	reorderedMovies = list(tiedMovies)
-	for m in tiedMovies:
-		if(str(m[1].year) in title):
-			reorderedMovies.remove(m)
-			reorderedMovies.insert(0, m)
-	for i in range(0, len(reorderedMovies)):
-		percentageMatchedMovieArray[i] = reorderedMovies[i]
-	return percentageMatchedMovieArray
+def resolveTies(percentageMatchedMovieArray, originalFileName):
+  tiedMovies = []
+  for i in range(0, len(percentageMatchedMovieArray)):
+    if i+1 < len(percentageMatchedMovieArray):
+      thisMovie = percentageMatchedMovieArray[i]
+      nextMovie = percentageMatchedMovieArray[i+1]
+      tiedMovies.append(thisMovie)
+      if thisMovie[0] != nextMovie[0]:
+        break
+  reorderedMovies = list(tiedMovies)
+  for m in tiedMovies:
+    if(str(m[1].year) in originalFileName):
+      reorderedMovies.remove(m)
+      reorderedMovies.insert(0, m)
+  for i in range(0, len(reorderedMovies)):
+    percentageMatchedMovieArray[i] = reorderedMovies[i]
+  return percentageMatchedMovieArray
 
 def percentageOfTitleMatch(firstTitle, secondTitle):
-	#Format the titles for comparison
-	firstArray = returnWellFormatedArrayFromTitle(firstTitle)
-	secondArray = returnWellFormatedArrayFromTitle(secondTitle)
+  #Format the titles for comparison
+  firstArray = returnWellFormatedArrayFromTitle(firstTitle)
+  secondArray = returnWellFormatedArrayFromTitle(secondTitle)
 
-	#Replace numerals
-	firstArray = replaceNumeralsInArray(firstArray)
-	secondArray = replaceNumeralsInArray(secondArray)
+  #Replace numerals
+  firstArray = replaceNumeralsInArray(firstArray)
+  secondArray = replaceNumeralsInArray(secondArray)
 
-	#Ensure the second array is always the larger of the two
-	if len(firstArray) > len(secondArray):
-		tempArray = firstArray
-		firstArray = secondArray
-		secondArray = tempArray
+  #Ensure the second array is always the larger of the two
+  if len(firstArray) > len(secondArray):
+    tempArray = firstArray
+    firstArray = secondArray
+    secondArray = tempArray
 
-	#Count how many words are in each title
-	lengthOfFirstTitle = len(firstArray)
-	lengthOfSecondTitle = len(secondArray)
+  #Count how many words are in each title
+  lengthOfFirstTitle = len(firstArray)
+  lengthOfSecondTitle = len(secondArray)
 
-	#Count the matching words between the two titles
-	matchingWordCount = 0
-	for word in firstArray:
-		if word in secondArray:
-			matchingWordCount += 1
-			secondArray.remove(word)
+  #Count the matching words between the two titles
+  matchingWordCount = 0
+  for word in firstArray:
+    if word in secondArray:
+      matchingWordCount += 1
+      secondArray.remove(word)
 
-	#Calculate the percentage of title match
-	percentFirstMatch = float(matchingWordCount) / lengthOfFirstTitle
-	percentSecondMatch = float(matchingWordCount) / lengthOfSecondTitle
-	return percentFirstMatch * percentSecondMatch
+  #Calculate the percentage of title match
+  percentFirstMatch = float(matchingWordCount) / lengthOfFirstTitle
+  percentSecondMatch = float(matchingWordCount) / lengthOfSecondTitle
+  return percentFirstMatch * percentSecondMatch
